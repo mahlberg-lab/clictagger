7956adebf7905c27065ca9f2799daeb3d762864d
Jamie Lentin <jm@lentin.co.uk>
2026-06-16 14:36:36 +0000

    __init__: pkg_resources -> importlib

--- clictagger/__init__.py
+++ clictagger/__init__.py
@@ -1,3 +1,8 @@
-import pkg_resources
+try:
+    import importlib.metadata
 
-__version__ = pkg_resources.get_distribution("clictagger").version
+    __version__ = importlib.metadata.version("clictagger")
+except ImportError:
+    import pkg_resources
+
+    __version__ = pkg_resources.get_distribution("clictagger").version
